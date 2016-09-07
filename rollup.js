b8b2c56f2f769daf4f19f8d9b85f8f84dcc3c859
Christian Potemski <c.potemski@gmail.com>
2016-09-07 11:23:50 +0200

    remove rollup file

--- rollup.js
+++
@@ -1,9 +0,0 @@
-const rollup = require('rollup')
-const rollupConfig = require('./rollup.config.js')
-
-let cache
-
-rollup(Object.assign({}, rollupConfig, { cache })).then(bundle => {
-  cache = bundle
-  bundle.write(rollupConfig)
-})
